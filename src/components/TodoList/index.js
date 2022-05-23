41c59da234167033fdcada675f563d4261510192
quangkhai76d1 <85468877+quangkhai76d1@users.noreply.github.com>
2022-05-23 11:25:30 +0700

    Update index.js
    update import

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from "uuid";
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import { newTodo } from "../../redux/selector";
-import { todoListSlice } from "./todoSlice";
+import { todoListSlice } from "./TodoSlice";
 
 export default function TodoList() {
   const dispatch = useDispatch();
